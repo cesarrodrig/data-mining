63fdeabe0e416115932728b44e75cfeb0f767fea
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2016-02-16 01:37:14 +0200

    First iteration

---
+++ assignment-4/main.py
@@ -0,0 +1,166 @@
+import os
+import csv
+import time
+from collections import defaultdict
+from itertools import combinations
+
+FILE_NAME = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data-2016.csv"))
+
+class Student(object):
+    def __init__(self, id, registration_year):
+        self.id = id
+        super(Student, self).__init__()
+        self.registration_year = registration_year
+        self.courses = {}
+
+    def add_course(self, course):
+        if course.name not in self.courses or course.final_grade > self.courses[course.name].final_grade:
+            self.courses[course.name] = course
+
+    def all_courses(self):
+        return [ course.code for course in self.courses.values() ]
+
+class CourseRecord(object):
+    def __init__(self, strings):
+        super(CourseRecord, self).__init__()
+        self.year_and_month = strings[0]
+        self.code = strings[1]
+        self.name = strings[2]
+        self.credits = strings[3]
+        self.final_grade = int(strings[4])
+
+students = []
+course_codes = set()
+inverted_index = defaultdict(set) # course to student ids
+
+with open(FILE_NAME, "rb") as csvfile:
+    reader = csv.reader(csvfile, delimiter=" ")
+
+    r = 0
+    for row in reader:
+        student = Student(r, row[0])
+
+        for i in range(1, len(row), 5):
+            course = CourseRecord(row[i:i+5])
+            student.add_course(course)
+            course_codes.add(course.code)
+            inverted_index[course.code].add(r)
+
+        r += 1
+        students.append(student)
+
+def support_count(transactions, itemset):
+    if type(itemset) not in (list, set, tuple):
+        raise Exception("Expected list or set, got %s" % type(itemset))
+    itemset = set(itemset)
+
+    curr_set = reduce(lambda x, y: x.intersection(y), [inverted_index[item] for item in itemset])
+    return len(curr_set)
+
+
+def support(transactions, itemset):
+    if type(itemset) not in (list, set, tuple):
+        raise Exception("Expected list or set, got %s" % type(itemset))
+    itemset = set(itemset)
+
+    supp_count = support_count(transactions, itemset)
+    return float(supp_count) / len(transactions)
+
+def merge(itemset_a, itemset_b):
+    # both are sorted lists, so we can use merge_sort method
+    merged = []
+    a, b = 0, 0
+    while a < len(itemset_a) and b < len(itemset_b):
+        if itemset_a[a] < itemset_b[b]:
+            merged.append(itemset_a[a])
+            a += 1
+        elif itemset_a[a] > itemset_b[b]:
+            merged.append(itemset_b[b])
+            b += 1
+        else:
+            merged.append(itemset_b[b])
+            a += 1
+            b += 1
+
+    while a < len(itemset_a):
+        merged.append(itemset_a[a])
+        a += 1
+
+    while b < len(itemset_b):
+        merged.append(itemset_b[b])
+        b += 1
+
+    return merged
+
+def generate(frequent):
+
+    candidates = []
+
+    if len(frequent) <= 1:
+        return candidates
+
+    index = 0
+    while index < len(frequent):
+        first = frequent[index]
+
+
+        index2 = index + 1
+        while index2 < len(frequent) and frequent[index2] != "BOUND":
+            second = frequent[index2]
+            candidates.append(merge(first, second))
+            index2 += 1
+
+        index += 1
+        candidates.append("BOUND")
+
+    return candidates
+
+def apriori(transactions, min_support, k=99999):
+
+    prev_result = []
+    for i, result in enumerate(apriori_generator(transactions, min_support)):
+        result = filter(lambda a: a!="BOUND", result)
+        if not result:
+            return prev_result
+        if i+1 == k:
+            return result
+
+        prev_result = result
+
+    return result
+
+
+def apriori_generator(transactions, min_support):
+    itemset = set([item for trans in transactions for item in trans ])
+
+    freq_itemsets = filter(lambda item: support(transactions, set([item])) >= min_support, itemset)
+    freq_itemsets = [[item] for item in freq_itemsets]
+
+    while freq_itemsets:
+        yield freq_itemsets
+        start_time = time.time()
+        candidate_itemsets = generate(freq_itemsets)
+        end_time = time.time()
+        next_freq_itemsets = []
+
+        for cand_itemset in candidate_itemsets:
+            if cand_itemset == "BOUND":
+                next_freq_itemsets.append(cand_itemset)
+            elif support(transactions, cand_itemset) >= min_support:
+                next_freq_itemsets.append(cand_itemset)
+
+        freq_itemsets = next_freq_itemsets
+
+course_transactions = [ student.all_courses() for student in students ]
+
+for i in range(3,5):
+    start_time = time.time()
+
+    combinations_high_supp = apriori(course_transactions, 0.1, i)
+    combinations_high_supp = filter(lambda a: a!="BOUND", combinations_high_supp)
+    # print combinations_high_supp
+    print len(combinations_high_supp), "combinations, %s seconds" % (time.time() - start_time)
+
+
+combinations_high_supp = apriori(course_transactions, 0.1, 10)
+print len(combinations_high_supp)
